056590f16d63600866e2002a0a17cd61a4318439
Иванов <ivnvegor@gmail.com>
2024-09-04 16:36:41 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def start():
-    return 'web-сервер на flask'
+    return '<!doctype html>'\
+        '<html>'\
+        '   <body>'\
+        '       <h1>web-сервер на flask</h1>'\
+        '   </body>'\
+        '</html>'
     
