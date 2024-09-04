17999bab540c7b590b5791311a037893545896de
Иванов <ivnvegor@gmail.com>
2024-09-04 17:56:10 +0700

    'Счетчик'

--- app.py
+++ app.py
@@ -42,4 +42,17 @@ def oak():
     </body>
 </html>
 '''
-    
+count = 0
+
+@app.route('/lab1/counter')
+def counter():
+    global count
+    count += 1
+    return '''
+<!doctype html>
+<html>
+    <body>
+        Сколько раз вы сюда заходили: ''' + str(count) + '''
+    </body>
+</html>
+'''
