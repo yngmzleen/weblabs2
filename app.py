ea226568f7fe225ddd6b8a568b17900d58c3faec
Иванов <ivnvegor@gmail.com>
2024-09-09 19:37:16 +0700

    стили

--- app.py
+++ app.py
@@ -42,12 +42,14 @@ def author():
 @app.route('/lab1/oak')
 def oak():
     path = url_for("static", filename = "oak.jpg")
+    style = url_for("static", filename = "lab1.css")
     return '''
 <!doctype html>
 <html>
+    <link rel = "stylesheet" href="''' + style +'''"
     <body>
         <h1>Дуб</h1>
-        <img src="''' + path + '''">  
+        <img src="''' + path + '''">
     </body>
 </html>
 '''
