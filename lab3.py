e74d0c197c856c1cf977fd18eb073e50a33a7712
Иванов <ivnvegor@gmail.com>
2024-10-02 18:35:13 +0700

    Cookie

--- lab3.py
+++ lab3.py
@@ -1,6 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, make_response, redirect
 lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
 def lab():
-    return render_template('lab3/lab3.html')
+    name = request.cookies.get('name')
+    name_color = request.cookies.get('name_color')
+    return render_template('lab3/lab3.html', name=name, name_color=name_color)
+
+@lab3.route('/lab3/cookie')
+def cookie():
+    resp = make_response(redirect('/lab3/'))
+    resp.set_cookie('name', 'Alex', max_age=5)
+    resp.set_cookie('age', '29')
+    resp.set_cookie('name_color', 'magenta')
+    return resp
+
+@lab3.route('/lab3/del_cookie')
+def del_cookie():
+    resp = make_response(redirect('/lab3/'))
+    resp.delete_cookie('name')
+    resp.delete_cookie('age')
+    resp.delete_cookie('name_color')
+    return resp
