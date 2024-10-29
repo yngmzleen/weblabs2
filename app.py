7bac47c63880218a27112ea4291071a8ef1856c5
Иванов <ivnvegor@gmail.com>
2024-10-29 18:51:05 +0700

    Деление

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, url_for, redirect, render_template
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 @app.errorhandler(404)
 def not_found(err):
@@ -41,6 +43,7 @@ def index():
                 <li><a href='/lab1'>Первая лабораторная</a></li>
                 <li><a href='/lab2/'>Вторая лабораторная</a></li>
                 <li><a href='/lab3/'>Третья лабораторная</a></li>
+                <li><a href='/lab4/'>Четвертая лабораторная</a></li>
            </body>
            <footer>Иванов Егор Владиславович, ФБИ-22, 3 курс, 2024</footer>
         </html>''', 200
---
+++ lab4.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template, request
+lab4 = Blueprint('lab4', __name__)
+
+@lab4.route('/lab4/')
+def lab():
+    return  render_template('lab4/lab4.html')
+
+@lab4.route('/lab4/div-form')
+def div_form():
+    return  render_template('lab4/div-form.html')
+
+@lab4.route('/lab4/div', methods = ['POST'])
+def div():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/div.html', error='Оба поля должны быть заполнены!')
+    if x2 == '0':
+        return render_template('lab4/div.html', error='На ноль делить нельзя!')
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 / x2
+    return render_template('lab4/div.html', x1=x1, x2=x2, result=result)    
