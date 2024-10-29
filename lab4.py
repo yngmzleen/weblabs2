4a4dce74d599bbdb7c33d9880383db4070778c6d
Иванов <ivnvegor@gmail.com>
2024-10-29 20:17:19 +0700

    авторизация

--- lab4.py
+++ lab4.py
@@ -62,3 +62,16 @@ def tree():
             tree_count = 10
     return redirect('/lab4/tree')
 
+@lab4.route('/lab4/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab4/login.html', authorized=False)
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if login == 'alex' and password == '123':       
+        return render_template('lab4/login.html', authorized=True, login=login)
+    
+    error = 'Неверные логин и/или пароль'
+    return render_template('lab4/login.html', authorized=False, error=error)
