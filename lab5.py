1170503aba307eb0fa657e449f6a3ce7bda4069f
Иванов <ivnvegor@gmail.com>
2024-11-26 18:35:09 +0700

    Создание статьи

--- lab5.py
+++ lab5.py
@@ -73,3 +73,23 @@ def login():
     session['login'] = login
     db_close(conn, cur)
     return render_template('lab5/success_login.html', login=login)
+
+@lab5.route('/lab5/create', methods=['GET', 'POST'])
+def create():
+    login=session.get('login')
+    if not login:
+        return redirect('/lab5/login')
+    
+    if request.method == 'GET':
+        return render_template('/lab5/create_article.html')
+    
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+    conn, cur = db_connect()
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
+    login_id = cur.fetchone()['id']
+    cur.execute(f"INSERT INTO articles(user_id, title, article_text) \
+                VALUES ({login_id}, '{title}', '{article_text}')")
+    
+    db_close(conn, cur)
+    return redirect('/lab5')
