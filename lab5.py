1cff61537f3ae9d7f9405b3afacb5ebe5ec11839
Иванов <ivnvegor@gmail.com>
2024-11-26 19:05:17 +0700

    Вывод статей

--- lab5.py
+++ lab5.py
@@ -93,3 +93,17 @@ def create():
     
     db_close(conn, cur)
     return redirect('/lab5')
+
+@lab5.route('/lab5/list')
+def list():
+    login = session.get('login')
+    if not login:
+        return redirect('/lab5/login')
+    
+    conn, cur = db_connect()
+    cur.execute(f"SELECT id FROM users WHERE login='{login}';")
+    login_id = cur.fetchone()['id']
+    cur.execute(f"SELECT * FROM articles WHERE user_id='{login_id}';")
+    articles = cur.fetchall()
+    db_close(conn, cur)
+    return render_template('/lab5/articles.html', articles=articles)
