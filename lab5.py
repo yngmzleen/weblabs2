6a8497b367d8927689ce33be8ea164d4b8303542
Иванов <ivnvegor@gmail.com>
2024-11-26 23:07:29 +0700

    правка (надеюсь последняя)

--- lab5.py
+++ lab5.py
@@ -92,28 +92,34 @@ def login():
 
 @lab5.route('/lab5/create', methods=['GET', 'POST'])
 def create():
-    login=session.get('login')
+    login = session.get('login')
     if not login:
         return redirect('/lab5/login')
-    
+
     if request.method == 'GET':
         return render_template('/lab5/create_article.html')
-    
+
     title = request.form.get('title')
     article_text = request.form.get('article_text')
+    is_public = request.form.get('is_public') == '1'
+
+    if not (title and article_text):
+        return render_template('/lab5/create_article.html', error='Введите текст и название статьи!')
+
     conn, cur = db_connect()
+
     if current_app.config['DB_TYPE'] == 'postgres':
-        cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
+        cur.execute("SELECT * FROM users WHERE login=%s;", (login,))
     else:
-        cur.execute("SELECT * FROM users WHERE login=?;", (login, ))
+        cur.execute("SELECT * FROM users WHERE login=?;", (login,))
+
     login_id = cur.fetchone()['id']
+
     if current_app.config['DB_TYPE'] == 'postgres':
-        cur.execute(f"INSERT INTO articles(user_id, title, article_text) \
-                    VALUES (%s, %s, %s);" , (login_id, title, article_text))
+        cur.execute("INSERT INTO articles(user_id, title, article_text, is_public) VALUES (%s, %s, %s, %s);", (login_id, title, article_text, is_public))
     else:
-        cur.execute(f"INSERT INTO articles(user_id, title, article_text) \
-                    VALUES (?, ?, ?);" , (login_id, title, article_text))
-    
+        cur.execute("INSERT INTO articles(login_id, title, article_text, is_public) VALUES (?, ?, ?, ?);", (login_id, title, article_text, is_public))
+
     db_close(conn, cur)
     return redirect('/lab5')
 
