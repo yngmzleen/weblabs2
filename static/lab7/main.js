46c13bb1ff1ef7b4e0f695d2969b4298cfa9d7ad
Иванов <ivnvegor@gmail.com>
2024-12-11 01:56:16 +0700

    Вывод таблицы

---
+++ static/lab7/main.js
@@ -0,0 +1,38 @@
+function fillFilmList() {
+    fetch('/lab7/rest-api/films/')
+    .then(function (data) {
+        return data.json();
+    })
+    .then(function (films) {
+        let tbody = document.getElementById('film-list');
+        tbody.innerHTML = '';
+        for(let i = 0; i<films.length; i++) {
+            let tr = document.createElement('tr');
+
+            let tdTitle = document.createElement('td');
+            let tdTitleRus = document.createElement('td');
+            let tdYear = document.createElement('td');
+            let tdActions = document.createElement('td');
+
+            tdTitle.innerText = films[i].title == films[i].title_ru ? ''  : films[i].title;
+            tdTitleRus.innerText = films[i].title_ru;
+            tdYear.innerText = films[i].year;
+
+            let editButton = document.createElement('button');
+            editButton.innerText = 'Редактировать';
+
+            let delButton = document.createElement('button');
+            delButton.innerText = 'Удалить';
+
+            tdActions.append(editButton);
+            tdActions.append(delButton);
+
+            tr.append(tdTitle);
+            tr.append(tdTitleRus);
+            tr.append(tdYear);
+            tr.append(tdActions);
+
+            tbody.append(tr);
+        }
+    })
+}
